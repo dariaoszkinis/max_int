bd14229d02a1903f083c6dee4423afbdf4243002
Daria Oszkinis <Daria@Daria.local>
2019-09-05 15:02:31 +0000

    Algorith writtem for sequence

---
+++ sequence.py
@@ -0,0 +1,15 @@
+n = int(input("Enter the length of the sequence:  "))
+
+count_number = 3
+num1 = 1
+num2 = 2
+num3 = 3
+
+while count_number <= n:
+    sum = num1 + num2 + num3
+    print(sum)
+    count_number += 1
+
+    num1 = num2
+    num2 = num3
+    num3 = sum
